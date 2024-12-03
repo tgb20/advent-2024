8006b2c8c03a9ec07612e6543fd7e11f3bbbedec
Jacob Bashista <tgb.bashista@gmail.com>
2024-12-03 08:47:33 -0500

    [DAY 3] Part 1

---
+++ day-3/part1.ts
@@ -0,0 +1,16 @@
+const text = await Deno.readTextFile("inputs/real.txt");
+
+const mulRegex = /(mul\([0-9]+,[0-9]+\))/g;
+
+const matchingMults = [...text.matchAll(mulRegex)].map((a) => a[0]);
+
+const digitRegex = /[0-9]+/g;
+
+let mulSum = 0;
+
+for (const mul of matchingMults) {
+  const digits = [...mul.matchAll(digitRegex)].map((a) => parseInt(a[0]));
+  mulSum += digits[0] * digits[1];
+}
+
+console.log(mulSum);
